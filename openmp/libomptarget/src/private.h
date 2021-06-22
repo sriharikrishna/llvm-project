8fb73fed1e3c38ff81d6a89aeb950e0487de8965
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2021-06-22 04:50:58 +0000

    Removes truct defined in Sourceinfo.h

--- openmp/libomptarget/src/private.h
+++ openmp/libomptarget/src/private.h
@@ -103,17 +103,6 @@ extern "C" {
 typedef int kmp_int32;
 typedef intptr_t kmp_intptr_t;
 
-typedef struct ident {
-  kmp_int32 reserved_1; /**<  might be used in Fortran; see above  */
-  kmp_int32 flags; /**<  also f.flags; KMP_IDENT_xxx flags; KMP_IDENT_KMPC
-                      identifies this union member  */
-  kmp_int32 reserved_2; /**<  not really used in Fortran any more; see above */
-  kmp_int32 reserved_3; /**<  source[4] in Fortran, do not use for C++  */
-  char const *psource; /**<  String describing the source location.
-                       The string is composed of semi-colon separated fields
-                       which describe the source file, the function and a pair
-                       of line numbers that delimit the construct. */
-} ident_t;
 // Compiler sends us this info:
 typedef struct kmp_depend_info {
   kmp_intptr_t base_addr;
